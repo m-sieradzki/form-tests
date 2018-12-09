80483aef379b743b932e2ad56684e4e00b51c8fa
m-sieradzki <maria@xxix.co>
2018-12-09 14:29:45 +0100

    Another test

--- canvas-upload.php
+++ canvas-upload.php
@@ -1,6 +1,6 @@
 <?php
 	// requires php5
-	define('UPLOAD_DIR', 'images/');
+	define('UPLOAD_DIR', 'uploads/');
 	$img = $_POST['img'];
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
